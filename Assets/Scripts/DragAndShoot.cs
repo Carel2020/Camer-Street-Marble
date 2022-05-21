4dbfebd27f8ba701523cafcb1bb16e5eb13f5ad5
Carel2020 <njankocarel@gmail.com>
2022-05-21 18:43:27 +0100

    I added the main menu

--- Assets/Scripts/DragAndShoot.cs
+++ Assets/Scripts/DragAndShoot.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class DragAndShoot : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector3 mousePressDownPos;
+    private Vector3 mouseReleasePos;
+
+    private Rigidbody rb;
+
+    private bool isShoot;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDown()
     {
-        
+        mousePressDownPos = Input.mousePosition;
+    }
+
+    private void OnMouseUp()
+    {
+        mouseReleasePos = Input.mousePosition;
+        Shoot(mouseReleasePos - mousePressDownPos);
+    }
+
+    private float forceMultiplier = 2;
+    void Shoot(Vector3 Force)
+    {
+        if (isShoot)
+            return;
+
+        rb.AddForce(new Vector3(Force.x, Force.y, Force.y) * forceMultiplier);
+        isShoot = true;
     }
 }
